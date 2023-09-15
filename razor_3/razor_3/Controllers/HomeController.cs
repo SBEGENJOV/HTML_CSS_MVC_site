34e32ac0ed2f087b9e8cd56a6358e222f41cad2d
SBEGENJOV <seydi19981995@gmail.com>
2023-09-15 10:59:59 +0300

    15.09.2023 11:00

---
+++ razor_3/razor_3/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace razor_3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Campus()
+        {
+            return View();
+        }
+        public ActionResult Basin()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
